323e72c27ab7c11e4c7bea8ead63cb932021e567
Matt Welke <mattwelke@gmail.com>
2022-04-23 02:51:33 -0400

    Fix build script to use seconds since epoch tag.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ SECONDS_SINCE_EPOCH=$(printf '%(%s)T\n' -1)
 TAG=$SECONDS_SINCE_EPOCH
 
 # Use Docker build args to pass GitHub Packages credentials to Gradle for build.
-docker build --build-arg gpr_user=$1 --build-arg gpr_key=$2 -t mwelke/openwhisk-runtime-java-18:1.0.0 .
+docker build --build-arg gpr_user=$1 --build-arg gpr_key=$2 -t mwelke/openwhisk-runtime-java-18:$TAG .
